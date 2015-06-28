3512b2c87a11d8bf0ef8f831a8b4596051f5a701
Boris Grinshpun <bg2178@columbia.edu>
2015-06-28 16:13:00 -0400

    fixed VJexpansion

--- simulation/src/run.py
+++ simulation/src/run.py
@@ -4,9 +4,9 @@ from diversitymeasures import make_hist
 import itertools
 
 k=-1.5
-a=500
+a=2000
 s=200000
-mm=10**4
+mm=10**5
 x,y=make_pwlaw(k,a,s)
 eq="y=%.1e*x^%.2g" % (a,k)
 repertoire=make_repertoire(x,y,650)
@@ -14,7 +14,7 @@ plot_abundance(range(1,x+1),y,eq,'fullrep',ymax=mm,col='r')
 print sum([i*j for i,j in zip(range(1,x+1),y)])
 #del y
 
-sample=sample_nb(repertoire,0.6,10)
+sample=sample_nb(repertoire,0.6,5)
 sample=list(itertools.chain(*sample))
 
 # convert sample into repertoire format <-- added to simulation code
@@ -29,11 +29,12 @@ y2=[len(j) for j in newrepertoire.values()]
 x2=newrepertoire.keys()
 
 plot_abundance(x2,y2,'nb','negative_bin',ymax=mm)
-#x=range(1,x+1)
+x=range(1,x+1)
 plot_abundance_pair(x,y,x2,y2,name='plotpair',ymax=mm)
 
 print sum([i*j for i,j in zip(x2,y2)])
 print sum(y2)
+print "\n"
 
 # Convert to diversity format
 def expand(rep):
@@ -41,9 +42,12 @@ def expand(rep):
 	expandedclones=list(itertools.chain(*expandedclones))
 
 	# i == clone id, n==copy no, j==VJid
-	expandedVJ=[[(n,j) for j in vj] for n,vj in rep.items()]
-	expandedVJ=list(itertools.chain(*expandedVJ))
-	expandedVJ=[(i,j,k) for i,(j,k) in enumerate(expandedVJ)]
+	expandedrep=[[(n,j) for j in vj] for n,vj in rep.items()]
+	expandedrep=list(itertools.chain(*expandedrep))
+	expandedrep=[(i,j,k) for i,(j,k) in enumerate(expandedrep)]
+	fullCDR3=[[[i,vj]]*n for i,n,vj in expandedrep]
+        expandedVJ=zip(*tuple(itertools.chain(*fullCDR3)))[1]
+	expandedVJ=zip(*make_hist(expandedVJ))[1]
 
 	return expandedclones, expandedVJ
 
@@ -54,7 +58,6 @@ Hvj=entropy(exVJ)
 
 print Hcdr3
 print Hvj
-
 
 exC,exVJ=expand(newrepertoire)
 Hcdr3=entropy(exC)
