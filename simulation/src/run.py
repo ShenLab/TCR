1b1c5bf6e89752c209753b5d65b36170cd2ae959
Boris Grinshpun <bg2178@columbia.edu>
2015-06-28 14:45:09 -0400

    updated run file

--- simulation/src/run.py
+++ simulation/src/run.py
@@ -4,17 +4,17 @@ from diversitymeasures import make_hist
 import itertools
 
 k=-1.5
-a=2000
+a=500
 s=200000
 mm=10**4
 x,y=make_pwlaw(k,a,s)
 eq="y=%.1e*x^%.2g" % (a,k)
 repertoire=make_repertoire(x,y,650)
-#plot_abundance(range(1,x+1),y,eq,'fullrep',ymax=mm,col='r')
+plot_abundance(range(1,x+1),y,eq,'fullrep',ymax=mm,col='r')
 print sum([i*j for i,j in zip(range(1,x+1),y)])
 #del y
 
-sample=sample_nb(repertoire,0.6,5)
+sample=sample_nb(repertoire,0.6,10)
 sample=list(itertools.chain(*sample))
 
 # convert sample into repertoire format <-- added to simulation code
@@ -28,9 +28,9 @@ if 0 in newrepertoire.keys():
 y2=[len(j) for j in newrepertoire.values()]
 x2=newrepertoire.keys()
 
-#plot_abundance(x2,y2,'nb','negative_bin',ymax=mm)
+plot_abundance(x2,y2,'nb','negative_bin',ymax=mm)
 #x=range(1,x+1)
-#plot_abundance_pair(x,y,x2,y2,name='plotpair',ymax=mm)
+plot_abundance_pair(x,y,x2,y2,name='plotpair',ymax=mm)
 
 print sum([i*j for i,j in zip(x2,y2)])
 print sum(y2)
