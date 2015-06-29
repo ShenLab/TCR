87c430e04b2855ac5d4757444fd69e5138c23a3b
Boris Grinshpun <bg2178@columbia.edu>
2015-06-29 00:39:57 -0400

    runfile

--- simulation/src/run.py
+++ simulation/src/run.py
@@ -4,17 +4,31 @@ from diversitymeasures import make_hist
 import itertools
 
 k=-1.5
-a=2000
-s=200000
+a=20000
+s=700000
 mm=10**5
 x,y=make_pwlaw(k,a,s)
 eq="y=%.1e*x^%.2g" % (a,k)
 repertoire=make_repertoire(x,y,650)
-plot_abundance(range(1,x+1),y,eq,'fullrep',ymax=mm,col='r')
-print sum([i*j for i,j in zip(range(1,x+1),y)])
-#del y
+x=xrange(1,x+1)
+#plot_abundance(range(1,x+1),y,eq,'fullrep',ymax=mm,col='r')
+print sum([i*j for i,j in zip(x,y)])
 
-sample=sample_nb(repertoire,0.6,5)
+frac_clones=0.0001
+cells_per_clone=0.7
+s=0.2
+expandedrepertoire=expand_repertoire(repertoire,frac_clones,cells_per_clone,s)
+
+if 0 in expandedrepertoire.keys():
+        expandedrepertoire.pop(0)
+
+y=[len(j) for j in expandedrepertoire.values()]
+x=expandedrepertoire.keys()
+
+plot_abundance(x,y,eq,'expanded',ymax=mm,col='r')
+print sum([i*j for i,j in zip(x,y)])
+
+sample=sample_nb(expandedrepertoire,0.6,5)
 sample=list(itertools.chain(*sample))
 
 # convert sample into repertoire format <-- added to simulation code
@@ -28,14 +42,16 @@ if 0 in newrepertoire.keys():
 y2=[len(j) for j in newrepertoire.values()]
 x2=newrepertoire.keys()
 
-plot_abundance(x2,y2,'nb','negative_bin',ymax=mm)
-x=range(1,x+1)
-plot_abundance_pair(x,y,x2,y2,name='plotpair',ymax=mm)
+#plot_abundance(x2,y2,'nb','negative_bin',ymax=mm)
+#x=range(1,x+1)
+plot_abundance_pair(x,y,x2,y2,name='plotpair_final',ymax=mm)
 
 print sum([i*j for i,j in zip(x2,y2)])
 print sum(y2)
 print "\n"
 
+
+exit()
 # Convert to diversity format
 def expand(rep):
 	expandedclones=[[i]*len(rep[i]) for i in rep.keys()]
