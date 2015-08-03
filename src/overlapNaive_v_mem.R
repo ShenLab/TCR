3e69c793d91a7100c09fdd4b4f05faa77c81eb7c
Boris Grinshpun <bg2178@c2b2ysmd8.arcs.cpmc.columbia.edu>
2015-08-03 07:30:43 -0400

    aggregates counts between replicates

--- src/overlapNaive_v_mem.R
+++ src/overlapNaive_v_mem.R
@@ -2,22 +2,36 @@ rm(list=ls())
 options(stringsAsFactors=F) 
 setwd('/Users/bg2178/DropboxCGC/FarberSamples/productive')
 
-#require(sm)
+require(plyr)
 
 donor<-'D201'
-type<-'CD8'
-tissue1='SP'
+type<-'CD4'
+tissue1='LLN'
 tissue2='ILN'
 incr=10
 
 L<-Sys.glob(paste(donor,"*",sep=''))
 L<-L[grep(type,L)]
+L<-L[grep(69,L,invert=T)]
 Lmem<-L[grep("tem",L,ignore.case=T)]
 Lnaive<-L[grep("naive",L,ignore.case=T)]
 
-# GET OVERLAP FOR MEM
-fmem1=read.table(Lmem[grep(tissue1,Lmem,ignore.case=T)],header=T)
-fmem2=read.table(Lmem[grep(tissue2,Lmem,ignore.case=T)],header=T)
+# GET OVERLAP FOR MEM GET OVERLAP FOR NAIVE AND MERGE REPLICATES
+L1=Lmem[grep(tissue1,Lmem,ignore.case=T)]
+fmem1=lapply(L1, function(f) read.table(f,header=T))
+fmem1=ldply(fmem1,rbind)
+Nf=data.frame(nucleotide=fmem1$nucleotide,count=fmem1$count)
+fmem1=aggregate(count ~ nucleotide, Nf, sum)
+fmem1=fmem1[order(-fmem1[,2]),]
+
+
+L2=Lmem[grep(tissue2,Lmem,ignore.case=T)]
+fmem2=lapply(L2, function(f) read.table(f,header=T))
+fmem2=ldply(fmem2,rbind)
+Nf=data.frame(nucleotide=fmem2$nucleotide,count=fmem2$count)
+fmem2=aggregate(count ~ nucleotide, Nf, sum)
+fmem2=fmem2[order(-fmem2[,2]),]
+
 fmem1$fq=fmem1$count/sum(fmem1$count)
 fmem2$fq=fmem2$count/sum(fmem2$count)
 
@@ -38,7 +52,6 @@ mem11fq=mem11counts/sum(fmem1$count)
 mem11fq=mem11fq[!is.na(mem11fq)]
 r11mem=r11mem[!is.na(r11mem)]
 
-sapply(seq(from=1, to=length(r2mem),by=(incr)), function(i) c(i, i+incr))
 r22mem=sapply(seq(from=1, to=length(r2mem),by=(incr)), function(i) sum(r2mem[i:(i+incr)])/length(unlist(mem2clones[i:(i+incr)])))
 mem22counts=sapply(seq(from=1, to=length(r2mem),by=(incr)),function(i) mean(mem2uniqcount[i:(i+incr)]))
 mem22fq=mem22counts/sum(fmem2$count)
@@ -61,9 +74,25 @@ r22mem=r22mem[!is.na(r22mem)]
 #plot(seqrange1,r1mem,log='x',pch=20)
 
 
-# GET OVERLAP FOR NAIVE
-fnaive1=read.table(Lnaive[grep(tissue1,Lnaive,ignore.case=T)][1],header=T)
-fnaive2=read.table(Lnaive[grep(tissue2,Lnaive,ignore.case=T)][1],header=T)
+# GET OVERLAP FOR NAIVE AND MERGE REPLICATES
+L1=Lnaive[grep(tissue1,Lnaive,ignore.case=T)]
+fnaive1=lapply(L1, function(f) read.table(f,header=T))
+fnaive1=ldply(fnaive1,rbind)
+Nf=data.frame(nucleotide=fnaive1$nucleotide,count=fnaive1$count)
+fnaive1=aggregate(count ~ nucleotide, Nf, sum)
+fnaive1=fnaive1[order(-fnaive1[,2]),]
+fnaive1=fnaive1[order(-fnaive1[,2]),]
+
+L2=Lnaive[grep(tissue2,Lnaive,ignore.case=T)]
+fnaive2=lapply(L2, function(f) read.table(f,header=T))
+fnaive2=ldply(fnaive2,rbind)
+Nf=data.frame(nucleotide=fnaive2$nucleotide,count=fnaive2$count)
+fnaive2=aggregate(count ~ nucleotide, Nf, sum)
+fnaive2=fnaive2[order(-fnaive2[,2]),]
+fnaive2=fnaive2[order(-fnaive2[,2]),]
+
+fnaive1$fq=fnaive1$count/sum(fnaive1$count)
+fnaive2$fq=fnaive2$count/sum(fnaive2$count)
 
 # separate by count
 naive1uniqcount=unique(fnaive1$count)
@@ -109,21 +138,17 @@ alloverlap=c(r11mem,r22mem,r11naive,r22naive)
 allfq=c(mem11fq,mem22fq,naive11fq,naive22fq)
 
 # PLOT
-#png(paste('/Users/bgrinshpun/DropboxCGC/circosgo/scatteroutput/',donor,'_',tissue1,'_',type,tissue2,'.png',sep=''))
-#par(bg=NA) 
+png(paste('/Users/bg2178/DropboxCGC/072715_JoePaper2/memnaive_scatter/',donor,'_',tissue1,'-',tissue2,'_',type,'.png',sep=''),pointsize=5,res=1000,height=2000,width=2000)
+par(bg=NA) 
 
-plot(mem11fq,r11mem,ylim=c(0,max(alloverlap)),xlim=c(min(allfq),max(allfq)*1.2),pch=20,cex.axis=1.4,cex=1.6,cex.lab=1.5,xlab="clone frequency",ylab="overlap frequency",log='x')
+plot(mem11fq,r11mem,ylim=c(0,max(alloverlap)),xlim=c(min(allfq),max(allfq)*1.2),main=paste(donor,", ",toupper(tissue1)," - ",toupper(tissue2),", ",type,sep=""),pch=20,cex.main=1.3,cex.axis=1.4,cex=1.6,cex.lab=1.5,xlab="clone frequency",ylab="overlap frequency",log='x')
 points(mem22fq,r22mem,pch=20,col='blue',cex=1.5)
 points(naive11fq,r11naive,pch=20,col='red',cex=1.5)
 points(naive22fq,r22naive,pch=20,col='green',cex=1.5)
 
 
-#plot(mem1uniqfq,r1mem,ylim=c(0,max(alloverlap)),xlim=c(min(allfq),max(allfq)*1.2),pch=20,xlab="clone frequency",ylab="overlap frequency",log='x')
-#points(mem2uniqfq,r2mem,col='blue',pch=20)
-#points(naive1uniqfq,r1naive,col='red',pch=18)
-#points(naive2uniqfq,r2naive,col='green',pch=18)
 legend('topleft',c(paste(tissue1,'mem',paste=''),
-                    paste(tissue2,'mem',paste=''),paste(tissue1,'naive',paste=''),paste(tissue2,'naive',paste='')),pch=20,col=c('black','blue','red','green'),cex=1.3, pt.cex=1.3)
-#dev.off()
+                    paste(tissue2,'mem',paste=''),paste(tissue1,'naive',paste=''),paste(tissue2,'naive',paste='')),pch=20,col=c('black','blue','red','green'),cex=1, pt.cex=1.3)
+dev.off()
 
 
